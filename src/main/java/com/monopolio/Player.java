54670e29147fd5689882b266d21ce7a83e3f8a91
TommyPatriarca <134707022+TommyPatriarca@users.noreply.github.com>
2024-04-22 12:29:51 +0200

    PlayerClass

---
+++ src/main/java/com/monopolio/Player.java
@@ -0,0 +1,23 @@
+package com.monopolio;
+
+public class Player {
+    public final String nome;
+    public int soldi = 1500;
+
+    public Player(String nome) {
+        this.nome = nome;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public int getSoldi(){
+        return soldi;
+    }
+
+    public void sottraiSoldi(int soldi) {
+        this.soldi = this.soldi-soldi;
+    }
+    public void aggiungiSoldi(int soldi) {
+        this.soldi = this.soldi+soldi;
+    }
+}
