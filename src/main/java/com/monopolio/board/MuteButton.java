9be0e142f522d41c6926ef77a17e74dd201f8982
TommyPatriarca <patriarcatommy@gmail.com>
2024-05-05 21:31:57 +0200

    UpdateMuteButton

--- src/main/java/com/monopolio/board/MuteButton.java
+++ src/main/java/com/monopolio/board/MuteButton.java
@@ -1,8 +1,13 @@
 package com.monopolio.board;
 
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
 
 import java.util.Objects;
 
@@ -13,6 +18,7 @@ public class MuteButton extends Button {
 
     public MuteButton() {
         this.muted = false;
+        design();
 
         // Carica le immagini per il pulsante play e il pulsante mute
         Image playImage = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/dices/rolldice.png")),50,50,true,true);
@@ -25,18 +31,22 @@ public class MuteButton extends Button {
         // Imposta l'icona di default a playIcon
         setGraphic(playIcon);
 
-        // Imposta il listener per il pulsante
+        // listener per il pulsante
         setOnAction(event -> {
-            muted = !muted; // Inverte lo stato del pulsante
-
-            // Cambia l'icona in base allo stato
             if (muted) {
                 setGraphic(muteIcon);
-                // Aggiungi qui la logica per disattivare l'audio
+                // logica per disattivare l'audio
             } else {
                 setGraphic(playIcon);
-                // Aggiungi qui la logica per attivare l'audio
+                // logica per attivare l'audio
             }
         });
+    }
+
+    public void design() {
+        setPrefSize(50, 50);
+        setBackground(new Background(new BackgroundFill(Color.web("#001233FF"), new CornerRadii(10), Insets.EMPTY)));
+        setTextFill(Color.WHITE);
+        setStyle(getStyle() + "-fx-background-color: #1081F9; -fx-text-fill: white; -fx-background-radius: 15;");
     }
 }
