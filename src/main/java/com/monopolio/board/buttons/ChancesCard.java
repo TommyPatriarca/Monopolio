058823fbad610ba58e2953ca0af840019ebf7184
TommyPatriarca <134707022+TommyPatriarca@users.noreply.github.com>
2024-05-06 14:42:46 +0200

    Colors

--- src/main/java/com/monopolio/board/buttons/ChancesCard.java
+++ src/main/java/com/monopolio/board/buttons/ChancesCard.java
@@ -37,40 +37,9 @@ public class ChancesCard extends Button {
         setPrefSize(110, 110);
         setBackground(new Background(new BackgroundFill(Color.web("#001233FF"), new CornerRadii(10), Insets.EMPTY)));
         setTextFill(Color.WHITE);
-        setStyle(getStyle() + "-fx-cursor: hand; -fx-border-radius: 0; -fx-border-width: 0px; -fx-font-weight: bold;");
+        setStyle(getStyle() + "-fx-cursor: hand; -fx-background-color: #f77f00; -fx-text-fill: white; -fx-background-radius: 15;");
     }
 
-    public int roll() {
-        value = RandUtils.Integer(1,6);
-
-        switch(value) {
-            case 1 -> {
-                img = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/dices/dice1.png")));
-            }
-            case 2 -> {
-                img = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/dices/dice2.png")));
-            }
-            case 3 -> {
-                img = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/dices/dice3.png")));
-            }
-            case 4 -> {
-                img = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/dices/dice4.png")));
-            }
-            case 5 -> {
-                img = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/dices/dice5.png")));
-            }
-            case 6 -> {
-                img = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/dices/dice6.png")));
-            }
-        }
-
-        view = new ImageView(img);
-        view.setFitHeight(80);
-        view.setPreserveRatio(true);
-        setGraphic(view);
-
-        return value;
-    }
 
     public ImageView getView() {
         return view;
